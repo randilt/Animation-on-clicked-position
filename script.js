f39f58c5639e78c003196ea2d62aab43b05db1cc
RandilT <randiltharusha72@gmail.com>
2023-12-27 12:41:19 +0530

    Onclick explision animation

---
+++ script.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
+
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    this.image = new Image();
+    this.image.src = "boom.png";
+    this.frame = 0;
+    this.timer = 0;
+  }
+  update() {
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+window.addEventListener("click", function (e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+});
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    // if (explosions[i].frame > 7) {
+    //   explosions.splice(i, 1);
+    // }
+  }
+  requestAnimationFrame(animate);
+};
+animate();
